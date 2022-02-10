a7d6b93be4b99cbcff592fe20bd2b59532af8865
wakaba0972 <benny.wu2199@gmail.com>
2022-02-10 23:26:07 +0800

    新增圖片

---
+++ index.js
@@ -0,0 +1,21 @@
+//const sharp = require('sharp');
+const express = require('express');
+//const Text2SVG = require('text-to-svg');
+const PORT = process.env.PORT || 3000;
+
+var app = express()
+    .use(express.json({limit : '500000kb'}))
+    .get('/randomImgs', (req, res)=> {
+        let num = Math.floor(Math.random() * 10)
+        if(num == 0){
+            if(Math.floor(Math.random() * 10) < 3) res.sendFile(__dirname + '/imgs/SSR_1.PNG')
+            else res.sendFile(__dirname + '/imgs/SSR_0.PNG')
+        }
+        else if(num < 4){
+            res.sendFile(__dirname + '/imgs/R_0.PNG')
+        }
+        else{ 
+            res.sendFile(__dirname + '/imgs/N_0.PNG')
+        }
+    })
+    .listen(PORT, ()=> console.log('Listening on ' + PORT))
