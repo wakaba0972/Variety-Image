0a9d3071cd15b939acb825904c5d49b43ce3c987
wakaba0972 <benny.wu2199@gmail.com>
2022-02-11 21:15:40 +0800

    hashtable

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-//const sharp = require('sharp');
 const express = require('express');
-//const Text2SVG = require('text-to-svg');
 const PORT = process.env.PORT || 3000;
+var map = new Map()
 
 var app = express()
     .use(express.json({limit : '50000kb'}))
     .use(express.static('./imgs'))
     .get('/', (req, res)=> {
-        res.send('go to /randomImgs')
-    })
-    .get('/ri', (req, res)=> {
-        let num = Math.floor(Math.random() * 10)
-        if(num == 0){
-            if(Math.floor(Math.random() * 10) < 3) res.redirect('SSR_1.png')
-            else res.redirect('SSR_0.PNG')
+        let data, seed = req.originalUrl.slice(2)
+        if(data = map.get(seed)){
+            let level = data['level']
+            let num = data['num']
+            res.redirect(level + '_' + num + '.jpg')
         }
-        else if(num < 4){
-            res.redirect('R_0.png')
+        else{
+            let rand = Math.floor(Math.random() * 10)
+            if(rand == 0){
+                if(Math.floor(Math.random() * 10) < 3){ 
+                    map.set(seed, {'level': 'SSR', 'num': 1})
+                    res.redirect('SSR_1.jpg')
+                }
+                else{ 
+                    map.set(seed, {'level': 'SSR', 'num': 0})
+                    res.redirect('SSR_0.jpg')
+                }
+            }
+            else if(rand < 4){
+                map.set(seed, {'level': 'R', 'num': 0})
+                res.redirect('R_0.jpg')
+            }
+            else{ 
+                map.set(seed, {'level': 'N', 'num': 0})
+                res.redirect('N_0.jpg')
+            }
         }
-        else{ 
-            res.redirect('N_0.PNG')
-        }
+        console.log(map)
     })
     .listen(PORT, ()=> console.log('Listening on ' + PORT))
